48c280b168e1c5629219fa8289b72fc000a7cfc1
David F <dfaure123@gmail.com>
2020-10-25 20:14:02 +0200

    New features according to specs

--- src/app/modules/users-table/pages/users-table.component.ts
+++ src/app/modules/users-table/pages/users-table.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {UsersService} from '../../../services/users.service';
 import {User} from '../../../models/user.model';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-users-table',
@@ -10,8 +11,14 @@ import {User} from '../../../models/user.model';
 export class UsersTableComponent implements OnInit {
 
   users: User[];
+  // original response from the API to re-init the search locally
+  usersFromApi: User[];
+  sortKey: string | null = null;
+  sortDirectionToggle: boolean | null = null;
+  searchValue = '';
 
-  constructor(private usersService: UsersService) { }
+  constructor(private usersService: UsersService, private router: Router) {
+  }
 
   ngOnInit(): void {
     this.getUsers();
@@ -19,7 +26,66 @@ export class UsersTableComponent implements OnInit {
 
   getUsers(): void {
     this.usersService.getUsers()
-      .subscribe(users => this.users = users);
+      .subscribe(users => {
+        this.users = [...users];
+        this.usersFromApi = [...users];
+      });
+  }
+
+  goToPosts(user: User): void {
+    this.router.navigate([`/posts`], {queryParams: {name: user.name, id: user.id}});
+  }
+
+  sortUsers(key: string): void {
+    this.sortKey = key;
+    this.sortDirectionToggle = !this.sortDirectionToggle;
+    this.users = this.users.sort((a, b) => {
+
+      let str1 = '';
+      let str2 = '';
+
+      if (this.sortKey.includes('.')) {
+        // convention: we can search to a deep of 1 only (like 'company.name')
+        const my2Keys = this.sortKey.split('.');
+        str1 = a[my2Keys[0]][my2Keys[1]];
+        str2 = b[my2Keys[0]][my2Keys[1]];
+      } else {
+        str1 = a[this.sortKey];
+        str2 = b[this.sortKey];
+      }
+
+      if (this.sortDirectionToggle) {
+        if (str1 < str2) {
+          return -1;
+        }
+        if (str1 > str2) {
+          return 1;
+        }
+      } else {
+        if (str1 > str2) {
+          return -1;
+        }
+        if (str1 < str2) {
+          return 1;
+        }
+      }
+      return 0;
+    });
+  }
+
+
+  filterUser(): void {
+    console.warn('Searching', this.searchValue);
+    if (this.searchValue === '') {
+      this.users = [...this.usersFromApi];
+    } else {
+      this.users = this.usersFromApi.filter(user => {
+        // TODO Currently only for name/username/email
+        return user.name.toLocaleLowerCase().includes(this.searchValue.toLowerCase())
+          || user.name.toLocaleLowerCase().includes(this.searchValue.toLowerCase())
+          || user.email.toLocaleLowerCase().includes(this.searchValue.toLowerCase());
+      });
+    }
   }
 
 }
--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -1,19 +1,46 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
-import {Observable} from 'rxjs';
+import {forkJoin, Observable, of, zip} from 'rxjs';
 import {User} from '../models/user.model';
+import {concatMap, map, tap} from 'rxjs/operators';
+import {Post} from '../models/post.model';
 
-export const usersUrl = 'https://jsonplaceholder.typicode.com/users';
+export const baseUrl = 'https://jsonplaceholder.typicode.com';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsersService {
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {
+  }
 
   /** GET heroes from the server */
   getUsers(): Observable<User[]> {
-    return this.http.get<User[]>(usersUrl);
+    return this.http.get<User[]>(`${baseUrl}/users`).pipe(
+      concatMap(users => {
+        const callAlbumsForUsers = users.map(user =>
+          zip(of(user), this.http.get(`${baseUrl}/users/${user.id}/albums`))
+        );
+        return forkJoin(...callAlbumsForUsers);
+      }),
+      map(resultForjoin => {
+        return resultForjoin.map(resultZip => {
+          const myUser = resultZip[0];
+          const hisNumerOfAlbum = resultZip[1].length;
+          myUser.numberOfAlbum = hisNumerOfAlbum;
+          return myUser;
+        });
+      }),
+      // TODO for quick debug only - to be removed
+      tap(users => console.log(users))
+    );
   }
+
+  getPosts = (userId: string): Observable<Post[]> =>
+    this.http.get<Post[]>(`${baseUrl}/posts?userId=${userId}`)
+      .pipe(
+        // TODO for quick debug only - to be removed
+        tap(posts => console.log(posts))
+      )
 }
