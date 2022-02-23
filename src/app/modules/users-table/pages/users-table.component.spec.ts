3c496f4a47d3d024ece0da41d8019c0d7d47bac1
David Faure <dfaure123@gmail.com>
2022-02-23 18:56:17 +0200

    clean tests

--- src/app/modules/users-table/pages/users-table.component.spec.ts
+++ src/app/modules/users-table/pages/users-table.component.spec.ts
@@ -1,16 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { of } from 'rxjs';
+import { UsersService } from 'src/app/services/users.service';
 import { UsersTableComponent } from './users-table.component';
 
 describe('UsersTableComponent', () => {
   let component: UsersTableComponent;
   let fixture: ComponentFixture<UsersTableComponent>;
+  let mockService: Partial<UsersService>;
 
   beforeEach(async () => {
+    mockService = { getUsers: () => of([]) };
+
     await TestBed.configureTestingModule({
-      declarations: [ UsersTableComponent ]
-    })
-    .compileComponents();
+      declarations: [UsersTableComponent],
+      providers: [{ provide: UsersService, useValue: mockService }],
+    }).compileComponents();
   });
 
   beforeEach(() => {
